4bea852a5467fa88c6c81e84465d511cb408d05e
Jacques Grillot <jacques@grillot.me>
2021-02-20 23:18:54 -0500

    Initial App with Install instructions

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # blinktOfficeNotifier
 Pi powered Home Office notification application with Pimoroni Blinkt
+
+Make an Office notification system using a Pi Zero W and a Pimoroni Blinkt LED strip. 
+
+## Installing
+This install was meant for a Pi Zero W on Raspian with no desktop environment. This assumes that the Pi already has the OS installed and configured. 
+
+1. Install the dependencies
+    1. Install Python3 Blinkt Library ```bash sudo apt install python3-blinkt ```
+    1. Install Flask ```bash sudo pip3 install flask ```
+1. Create a Crontab job
+1. Add this line to the end of the file, set the path to where you downloaded the Python app @reboot python3 /home/pi/Github/blinktOfficeNotifier
+1. Save and Exit
+1. By default, the app will run on the 5000 port of the Pi. http://yourpi.lan:5000
+
+
---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import *
+from blinkt import set_all, set_brightness, show, clear
+import time
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+@app.route('/off')
+def off():
+    clear()
+    show()
+    return render_template('index.html')
+@app.route('/video')
+def video():
+    set_all(255,0,0,brightness=0.1)
+    show()
+    return render_template('index.html')
+@app.route('/meeting')
+def meeting():
+    set_all(255,255,0,brightness=0.1)
+    show()
+    return render_template('index.html')
+@app.route('/working')
+def working():
+    set_all(0,255,0,brightness=0.1)
+    show()
+    return render_template('index.html')
+@app.route('/done')
+def done():
+    set_all(0,0,255,brightness=0.1)
+    show()
+    return render_template('index.html')
+if __name__ == "__main__":
